26cb641beba86ba1ba9cd59b98424c1110d13c10
Melech Mizrachi <mmizrachi@blitzagency.com>
2012-09-10 14:27:53 -0700

    New Files Uploaded

---
+++ Test/.bash_profile
@@ -0,0 +1,5 @@
+# load .profile, contaning login, non-bash related initialization
+source ~/.profile
+
+# load .bashrc, containing non-login related bash initialization
+source ~/.bashrc
---
+++ Test/Hello-Mel.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Hello Mel<br /><br />";
+
+for ($i=0;$i<10;$i++) {
+	echo "Hi ".$i."<br />";
+}
+
+echo "Another save test!<br />";
+
+echo "Hello Again! With a new Br.<br />";
+
+?>
